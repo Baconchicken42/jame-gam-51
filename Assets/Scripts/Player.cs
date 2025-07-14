9178022ad3b9488eb7c0d8f4a14afb5f2cc431d1
Lucas Thielmann <darthsidious@earthlink.net>
2025-07-13 20:40:39 -0400

    added sprinting and reduced player speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,10 @@ public class Player : MonoBehaviour
 {
     public InputActionReference moveAction;
     public InputActionReference interactAction;
+    public InputActionReference sprintAction;
 
     public float movementSpeed = 10.0f;
+    public float sprintMultiplier = 1.75f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,12 +17,13 @@ public class Player : MonoBehaviour
     {
         moveAction.action.Enable();
         interactAction.action.Enable();
+        sprintAction.action.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveAxis = moveAction.action.ReadValue<Vector2>();
-        transform.position += new Vector3(moveAxis.x, 0, moveAxis.y) * movementSpeed * Time.deltaTime;
+        Vector2 moveAxis = (sprintAction.action.IsPressed()) ? moveAction.action.ReadValue<Vector2>() * sprintMultiplier : moveAction.action.ReadValue<Vector2>();
+        transform.position += new Vector3(moveAxis.x, 0, moveAxis.y) * movementSpeed * sprintMultiplier * Time.deltaTime;
     }
 }
