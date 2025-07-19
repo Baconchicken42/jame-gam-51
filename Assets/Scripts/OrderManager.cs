f41b4060301104045068b39599aa2e1f432132cd
Lucas Thielmann <darthsidious@earthlink.net>
2025-07-18 21:54:07 -0400

    set up ordermanager logic

--- Assets/Scripts/OrderManager.cs
+++ Assets/Scripts/OrderManager.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class OrderManager : MonoBehaviour
@@ -7,12 +8,40 @@ public class OrderManager : MonoBehaviour
     [SerializeField]
     public Order[] orders;
 
-    private Employee[] employees;
+    public Employee[] employees;
+    private float timer = 0f;
 
 
     private void Start()
     {
         employees = FindObjectsByType<Employee>(FindObjectsSortMode.None);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        //not gonna bother trying to avoid looping every frame, this game will not be very performance intensive and I don't want to require orders to be sorted and invite human error
+        for (int j = 0; j < orders.Length; j++)
+        {
+            if (!orders[j].wasDispensed && timer >= orders[j].activationTimeSeconds)
+            {
+                for (int i = 0; i < employees.Length; i++)
+                {
+                    if (employees[i].id == orders[j].employeeID)
+                    {
+                        //not really sure if this will pass by reference or not so making a duplicate just to be certain
+                        employees[i].orders.Add(new Order(orders[j].employeeID, orders[j].color, orders[j].type, orders[j].timeRequired, orders[j].activationTimeSeconds));
+                        foreach (Order item in employees[i].orders)
+                        {
+                            Debug.Log(item.type);
+                        }
+                        orders[j].wasDispensed = true;
+                        Debug.Log($"Order was dispensed to Employee {employees[i].id}");
+                    }
+                }
+            }
+        }
     }
 
 }
@@ -28,4 +57,13 @@ public class Order
 
     [HideInInspector]
     public bool wasDispensed;
+
+    public Order(int myId, documentColor myColor, documentType myType, float myTimeRequired, float myActivationTime)
+    {
+        employeeID = myId;
+        color = myColor;
+        type = myType;
+        timeRequired = myTimeRequired;
+        activationTimeSeconds = myActivationTime;
+    }
 }
